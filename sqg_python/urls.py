26bd7bb9d828d4a6ad208813112ad5cc438a0f02
Sandeep Yadav <sy895090@gmail.com>
2019-05-24 00:23:53 +0530

    Added url of quality-leads application to the main url

--- sqg_python/urls.py
+++ sqg_python/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the quality_leads application 
+from django.urls import include
+from django.urls import path
+urlpatterns += [
+    path('quality_leads/', include('quality_leads.urls')),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
